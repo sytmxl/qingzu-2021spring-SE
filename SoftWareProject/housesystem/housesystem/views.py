cc49d49c4e0e38a9abc19e8523fad19ac5c0dc18
Rooopz <8114150+lxyxiaoh@user.noreply.gitee.com>
2022-05-06 11:19:16 +0000

    ?

---
+++ SoftWareProject/housesystem/housesystem/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+# Create your views here.
+'''
+主界面网站：
+根据用户的点击，选择跳转的网站
+'''
+def index(request):
+
+    return JsonResponse()
---
+++ SoftWareProject/housesystem/user/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+'''
+from user.models import ...
+类在这里注册
+'''
+# Register your models here.
