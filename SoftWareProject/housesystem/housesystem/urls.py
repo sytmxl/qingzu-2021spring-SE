3cb7b06bfbdcc313e79653a2903e4cbf1dc2097e
sytmxl <1728999839@qq.com>
2022-06-09 23:22:07 +0800

    server ver

--- SoftWareProject/housesystem/housesystem/urls.py
+++ SoftWareProject/housesystem/housesystem/urls.py
@@ -21,29 +21,29 @@ import index.views
 import user.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('FirstPage/', index.views.FirstPage), #主界面网站
-    path('search/',index.views.search),
-    path('order/',index.views.order),
-    path('info_order/',index.views.info_order),
-    path('service/',index.views.service),
-    path('info_complain/',index.views.info_complain),
-    path('connect/',index.views.connect),
-    path('collection/',index.views.collection),
-    path('information/',index.views.information),
-    path('user/',user.views.user),
-    path('Login/', user.views.Login), #和用户有关的网址，注册登录个人信息等
-    path('Register/',user.views.Register),
-    path('RepairMan_SelfInfo/',user.views.RepairMan_SelfInfo),
-    path('History_Work/',user.views.History_Work),
-    path('Todo_Work/',user.views.Todo_Work),
-    path('Commander_FirstPage/',user.views.Commander_FirstPage),
-    path('Manage_User/',user.views.Manage_User),
-    path('Manage_House/',user.views.Manage_House),
-    path('Manage_RM/',user.views.Manage_RM),
-    path('Manage_Contract/',user.views.Manage_Contract),
-    path('UnManaged_Contract/',user.views.UnManaged_Contract),
-    path('Manage_Complain/',user.views.Manage_Complain),
-    path('Managed_Complain/',user.views.Managed_Complain),
+    path('api/admin/', admin.site.urls),
+    path('api/FirstPage/', index.views.FirstPage), #主界面网站
+    path('api/search/',index.views.search),
+    path('api/order/',index.views.order),
+    path('api/info_order/',index.views.info_order),
+    path('api/service/',index.views.service),
+    path('api/info_complain/',index.views.info_complain),
+    path('api/connect/',index.views.connect),
+    path('api/collection/',index.views.collection),
+    path('api/information/',index.views.information),
+    path('api/user/',user.views.user),
+    path('api/Login/', user.views.Login), #和用户有关的网址，注册登录个人信息等
+    path('api/Register/',user.views.Register),
+    path('api/RepairMan_SelfInfo/',user.views.RepairMan_SelfInfo),
+    path('api/History_Work/',user.views.History_Work),
+    path('api/Todo_Work/',user.views.Todo_Work),
+    path('api/Commander_FirstPage/',user.views.Commander_FirstPage),
+    path('api/Manage_User/',user.views.Manage_User),
+    path('api/Manage_House/',user.views.Manage_House),
+    path('api/Manage_RM/',user.views.Manage_RM),
+    path('api/Manage_Contract/',user.views.Manage_Contract),
+    path('api/UnManaged_Contract/',user.views.UnManaged_Contract),
+    path('api/Manage_Complain/',user.views.Manage_Complain),
+    path('api/Managed_Complain/',user.views.Managed_Complain),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
